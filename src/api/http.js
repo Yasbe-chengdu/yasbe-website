1dc31cf5a2fcf455b7661ce2bf7bad3de30e29a3
你最珍贵 <9479144+guoguanrong@user.noreply.gitee.com>
2026-07-03 16:58:39 +0800

    test

--- src/api/http.js
+++ src/api/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const defaultApiBaseUrl = import.meta.env.MODE === 'production' ? 'https://api.yasbe.com' : 'https://api.beeznis.com'
 
+
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || defaultApiBaseUrl,
   timeout: 15000,
